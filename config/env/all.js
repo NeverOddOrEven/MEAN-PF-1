90d6405e3e2de522f82135ee60b46e0ebfbaa2dc
Alex S <asuttmiller@gmail.com>
2014-06-30 22:01:20 -0400

    Removed Bootstrap - Content Links now absolutely positioned with media query

--- config/env/all.js
+++ config/env/all.js
@@ -13,8 +13,6 @@ module.exports = {
 	assets: {
 		lib: {
 			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
 			],
 			js: [
 				'public/lib/angular/angular.js',
@@ -22,8 +20,7 @@ module.exports = {
 				'public/lib/angular-resource/angular-resource.js',
 				'public/lib/angular-animate/angular-animate.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
-				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-ui-utils/ui-utils.js'
 			]
 		},
 		css: [
