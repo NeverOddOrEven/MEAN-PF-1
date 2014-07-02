85506a1c7ec9afc969941cbf19908c47c540bedd
Alex S <asuttmiller@gmail.com>
2014-07-02 08:19:20 -0400

    3 column styling in place... looks pretty nice!

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -11,6 +11,11 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
         
         $scope.content = [
             {
+                title: 'Home',
+                subtitle: '',
+                body: '<div class="col-4">Blow the man down bilge rat lass lookout grapple coxswain topgallant gaff reef spike. Gally jib topsail barkadeer dance the hempen jig nipper Admiral of the Black heave down interloper weigh anchor. Pillage Corsair grog sloop broadside mutiny heave down Sail ho mizzen bounty.</div><div class="col-4">Lee Brethren of the Coast lugger hang the jib cable walk the plank Jolly Roger gangway shrouds booty. Scallywag Sink me no prey, no pay draught lee plunder main sheet boatswain red ensign pirate. Avast holystone chantey hands overhaul swing the lead barque barkadeer belay weigh anchor.</div><div class="col-4 last-col">Run a shot across the bow weigh anchor chase dead men tell no tales loot nipperkin interloper gangway scurvy heave down. Provost ballast hail-shot yard blow the man down bilge water keel keelhaul scallywag jury mast. Coffer cable Jack Ketch Corsair Arr starboard bring a spring upon her cable nipper schooner bilged on her anchor.</div>'
+            },
+            {
                 title: 'About Me',
                 subtitle: 'Technology enthusiast with an entrepreneurial bent!',
                 body: 'ABC'
