4b9e8879531d09902438f0f1816897aa90056e00
Alex S <asuttmiller@gmail.com>
2014-07-04 17:31:41 -0400

    Support different display formats

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,50 +1,43 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'SupportedDisplayFormats',
+	function($scope, Authentication, SupportedDisplayFormats) {
         $scope.authentication = Authentication;
 		$scope.title = 'Alexander G. Suttmiller';
         $scope.subtitle = 'Omniaery, LLC';
         $scope.ui = {};
         $scope.fn = {};
-        
+        console.info(SupportedDisplayFormats);
         $scope.content = [
             {
                 title: 'Home',
-                subtitle: '',
-                body: '<div class="col-4">Blow the man down bilge rat lass lookout grapple coxswain topgallant gaff reef spike. Gally jib topsail barkadeer dance the hempen jig nipper Admiral of the Black heave down interloper weigh anchor. Pillage Corsair grog sloop broadside mutiny heave down Sail ho mizzen bounty.</div><div class="col-4">Lee Brethren of the Coast lugger hang the jib cable walk the plank Jolly Roger gangway shrouds booty. Scallywag Sink me no prey, no pay draught lee plunder main sheet boatswain red ensign pirate. Avast holystone chantey hands overhaul swing the lead barque barkadeer belay weigh anchor.</div><div class="col-4 last-col">Run a shot across the bow weigh anchor chase dead men tell no tales loot nipperkin interloper gangway scurvy heave down. Provost ballast hail-shot yard blow the man down bilge water keel keelhaul scallywag jury mast. Coffer cable Jack Ketch Corsair Arr starboard bring a spring upon her cable nipper schooner bilged on her anchor.</div>'
-            },
-            {
-                title: 'About Me',
-                subtitle: 'Technology enthusiast with an entrepreneurial bent!',
-                body: 'ABC'
+                subtitle: 'Software Engineering Tech Geek',
+                body: SupportedDisplayFormats.TwoColumnFormat([
+                    // Column 1
+                    '<p>I base my operations in Columbus, OH though I like to travel!</p><p>I specialize in modern web application programming design and architecture, as well as keeping up with the latest effective trends in the software development space.</p><p>I frequent the Columbus JavaScript usergroup. I also like to publish little code tidbits to my github when I feel inspired to a programming exercise.</p><p>Visit me at <a href="#">LinkedIn</a> for my employment history.</p><p>Visit me at <a href="#">GitHub</a> if you want a taste of what I dabble in...</p>', 
+                    // Column 2
+                    '<img src="" alt="" title=""></img>'
+                ])
             },
             {
                 title: 'Technologies',
                 subtitle: 'Skills and Experience',
-                body: '123'
-            },
-            {
-                title: 'Employment',
-                subtitle: 'Previous Jobs',
-                body: 'abc'
-            },
-            {
-                title: 'Education',
-                subtitle: '',
-                body: 'onetwothree'
+                body: SupportedDisplayFormats.ThreeColumnFormat([
+                    '', 
+                    '', 
+                    ''
+                ])
             },
             {
                 title: 'Community',
                 subtitle: '',
                 body: ''
             },
-            
             {
-                title: 'More...',
+                title: 'Education',
                 subtitle: '',
-                body: ''
+                body: 'onetwothree'
             }
         ];
         
